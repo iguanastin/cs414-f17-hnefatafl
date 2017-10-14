e260fcbb41f22b6920e54a5315a75db146860352
iguanastin <austinthompson194@gmail.com>
2017-10-14 00:16:47 -0600

    Added javadoc to major components, implemented really basic User, created login events (unimplemented)

--- src/main/java/client/Client.java
+++ src/main/java/client/Client.java
@@ -1,9 +1,7 @@
 package client;
 
 
-import common.ClientDisconnectEvent;
-import common.ConnectAcceptedEvent;
-import common.HeartbeatEvent;
+import common.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -11,9 +9,19 @@ import java.io.IOException;
 
 public class Client extends AbstractClient {
 
-    final Logger logger = LoggerFactory.getLogger(Client.class);
+    /**
+     * SLF$J Logger for logging info for this client
+     */
+    private final Logger logger = LoggerFactory.getLogger(Client.class);
 
 
+    /**
+     * Constructs a client and attempts to connect it to the given host:port
+     *
+     * @param host Hostname/IP of the server
+     * @param port Port of the server
+     * @throws IOException When the server does not exist on the given host:port
+     */
     public Client(String host, int port) throws IOException {
         super(host, port);
         logger.info("Connecting to server at " + host + ":" + port + "...");
@@ -22,18 +30,41 @@ public class Client extends AbstractClient {
         logger.info("Connected!");
     }
 
+    /**
+     * Central inlet for all messages received from the server. Called each time the server sends a message.
+     *
+     * @param msg the message sent.
+     */
     @Override
     protected void handleMessageFromServer(Object msg) {
         logger.info("[SERVER]: " + msg);
 
-        if (msg instanceof HeartbeatEvent) {
-            handleHeartbeat(msg);
+        if (msg instanceof Event) handleEventFromServer((Event) msg);
+    }
+
+    /**
+     * Central inlet for all events received from the server. Called each time the server send an event.
+     *
+     * @param event Event that the server sent.
+     */
+    private void handleEventFromServer(Event event) {
+        if (event instanceof HeartbeatEvent) {
+            handleHeartbeat(event);
+        } else if (event instanceof LoginFailedEvent) {
+            //TODO: Handle login attempt failed
+        } else if (event instanceof LoginSuccessEvent) {
+            //TODO: Handle login attempt succeeded
         }
     }
 
-    private void handleHeartbeat(Object msg) {
+    /**
+     * Sends the heartbeat event back to the server. If sending fails, disconnects the client.
+     *
+     * @param event HeartbeatEvent received from the server.
+     */
+    private void handleHeartbeat(Event event) {
         try {
-            sendToServer(msg);
+            sendToServer(event);
         } catch (IOException e) {
             logger.error("Error sending response heartbeat to server", e);
             try {
@@ -43,6 +74,11 @@ public class Client extends AbstractClient {
         }
     }
 
+    /**
+     * Disconnects this client cleanly from the server.
+     *
+     * @throws IOException If the server is already disconnected.
+     */
     public synchronized void disconnect() throws IOException {
         sendToServer(new ClientDisconnectEvent());
         closeConnection();
---
+++ src/main/java/server/User.java
@@ -0,0 +1,78 @@
+package server;
+
+
+public class User {
+
+    /**
+     * The connection currently associated with the user. Null if the user is not logged in.
+     */
+    private ConnectionToClient client = null;
+
+    /**
+     * Users unique name
+     */
+    private final String name;
+
+    /**
+     * Users password for login
+     */
+    private String password;
+    //TODO: Make password hashed and salted
+
+
+    /**
+     * Constructs a user object to represent a user in the system with a given set of credentials.
+     *
+     * @param name Unique name of the user
+     * @param password Password for this account
+     */
+    public User(String name, String password) {
+        this.name = name;
+        this.password = password;
+    }
+
+    /**
+     * Tests credentials against this user.
+     *
+     * @param pass The password being tested against this user.
+     * @return True if the password matches this user; false otherwise.
+     */
+    public boolean comparePassword(String pass) {
+        //TODO: Compare hashed and salted password instead of plaintext
+        return this.password.equals(pass);
+    }
+
+    /**
+     *
+     * @return This user's unique name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     *
+     * @return The client connection for this user if present. Null if the user is not logged in.
+     */
+    public ConnectionToClient getClient() {
+        return client;
+    }
+
+    /**
+     * Sets the client connection.
+     *
+     * @param client Client connection
+     */
+    public void setClient(ConnectionToClient client) {
+        this.client = client;
+    }
+
+    /**
+     *
+     * @return True if this user is currently logged in; false otherwise.
+     */
+    public boolean isLoggedIn() {
+        return getClient() != null;
+    }
+
+}
