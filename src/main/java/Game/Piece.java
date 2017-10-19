0d907924fd21fc18e9ba6a84ad27f48db38a2d94
TheChemtrail <chris.yancey96@gmail.com>
2017-10-18 18:36:04 -0600

    Add files via upload

---
+++ src/main/java/Game/Piece.java
@@ -0,0 +1,29 @@
+package Game;
+
+enum Color {
+	BLACK, WHITE
+}
+public class Piece {
+	//Contains owner of piece
+	private User user;
+	private boolean isKing;
+	private Color color;
+	
+	public Piece(User user, Color color, boolean isKing) {
+		this.user = user;
+		this.isKing = isKing;
+		this.color = color;
+	}
+	public User getUser() {
+		return user;
+	}
+	public boolean isKing() {
+		return isKing;
+	}
+	public Color getColor() {
+		return color;
+	}
+	public String toString() {
+		return "isKing:" + isKing + " Color:" + color;
+	}
+}
